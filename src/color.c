922c556a6e2a41879fd19a40a806e32226a633ff
Adnan Mouhandiz <amouhand@c4r8p10.1337.ma>
2024-05-23 20:17:56 +0100

    nice

--- src/color.c
+++ src/color.c
@@ -6,24 +6,13 @@
 /*   By: amouhand <amouhand@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 00:50:13 by amouhand          #+#    #+#             */
-/*   Updated: 2024/04/23 01:51:06 by amouhand         ###   ########.fr       */
+/*   Updated: 2024/05/23 20:06:29 by amouhand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 #include <stdlib.h>
 
-int	random_color(void)
-{
-	int	r;
-	int	g;
-	int	b;
-
-	r = rand() % 256;
-	g = rand() % 256;
-	b = rand() % 256;
-	return ((r << 16) | (g << 8) | b);
-}
 int	color(t_mlx *fractol, int iter)
 {
 	int	r;
